5bdb5c02fc87199b98da877c2f85db86047470a8
James Bart <jbart1@gmail.com>
2025-07-07 15:10:02 +0100

    First push

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
 # mpv-save-position
 A cross platform script to save a files position when using mpv or mpv android
+
+
+# [mpv Save Position]
+
+A cross platform script to save a files position when using mpv or mpv android cross devices
+A simple utility that uses MD5 hashing to process filenames (or other strings) for purposes like caching, unique identification, or obfuscation.
+
+## Features
+
+- Generate MD5 hashes from filenames or strings
+- Easy-to-use script with minimal dependencies
+- Useful for cache keys, data deduplication, or anonymized identifiers
+
+## Requirements
+
+- mpv or mpv-android
+
+## 🚀 Usage
+
+### Save location
+- You can specify the location of where the save positions of files are stored by editing the locations in 'saveposition.lua' below
+    - linux_folder
+    - windows_folder
+    - android_folder
+
+### Linux
+Copy all lua files to  /home/username/.config/mpv/scripts
+
+### Android 
+- NOTE: Since Android version 12 Google has restricted access to the data folder where the files need to be placed so please see the guide below
+
+
+
+### Installation guide (non-rooted)
+
+- In the Google Play Store install the files app 
+
+[Files app (Play Store)](/sdcard/Android/data/is.xyz.mpv/files/.config/mpv/scripts/)
+
+
+- Open the files App and navigate to /sdcard/Android/data/is.xyz.mpv/files/
+- In this folder create the folders .config->mpv->scripts
+- Copy the Lua files for this script to your phone (any location)
+- NOTE: Using the Files App you will only be able to copy the files initially to the 'Android' foldeer
+- For each Lua file open the Files app and select each file, select the three dots on the top right and select 'copy to'
+- Copy each file to the 'Android' folder
+- Then in the Files app open the Android folder
+- You should now be able to see the 'data' folder
+- Drag each Lua file into data->is.xyz.mpv->files->.config->mpv->scripts
+
+- Open mpv-android
+    - Press back button once
+    - Go to Settings->Advanced - edit mpv.conf
+    - add the following: script="/storage/emulated/0/Android/data/is.xyz.mpv/files/.config/mpv/scripts/saveposition.lua"
+
+### Installation guide (Rooted)
+- Copy each Lua file into data->is.xyz.mpv->files->.config->mpv->scripts
+- Open mpv-android
+    - Press back button once
+    - Go to Settings->Advanced - edit mpv.conf
+    - add the following: script="/storage/emulated/0/Android/data/is.xyz.mpv/files/.config/mpv/scripts/saveposition.lua"
+
+## Dependencies / Acknowledgements
+This project uses [**md5.lua**](https://github.com/kikito/md5.lua) by [@kikito](https://github.com/kikito), a pure Lua implementation of the MD5 message-digest algorithm.
+> md5.lua is licensed under the MIT License.  
+> https://github.com/kikito/md5.lua
---
+++ saveposition.lua
@@ -0,0 +1,102 @@
+filepath = ''
+folder = ''
+duration = 0
+position = 0
+dkjson = nil
+isPlaying = true
+
+
+mp.register_event("file-loaded", function()
+
+    myos = getOS()
+    filename = getFilename(myos)
+    filename = string.gsub(filename, "[^%w%.%-_]", "_")
+    duration = mp.get_property_number("duration")
+    if myos == 'GNU/Linux' or os == 'OSX' or os =='Darwin' then
+        linux_folder = os.getenv("HOME") .. "/Documents/mpv-positions/"
+        dkjson_file = os.getenv( "HOME" ) .. '/.config/mpv/scripts/dkjson.lua'
+        dkjson = loadfile(dkjson_file)()
+        folder = linux_folder
+    end
+    if myos == "Android" or myos == "Toybox" then
+        android_folder = "/storage/emulated/0/Android/media/is.xyz.mpv/mpv-positions/"
+        dkjson = loadfile('/storage/emulated/0/Android/data/is.xyz.mpv/files/.config/mpv/scripts/dkjson.lua')()
+        folder = android_folder
+    end
+    if myos == "Windows" then
+        windows_folder = ""
+        folder = windows_folder
+    end
+    filepath = folder .. filename .. ".json"
+    filepath = folder .. filename .. ".json"
+    local positionFile, err = io.open(filepath, "r")
+    if not positionFile then
+        print("Could not open position file for reading:", err)
+        return
+    else
+        local content, err = positionFile:read("*all")
+        local data, pos, err = dkjson.decode(content, 1, nil)
+        if err then
+            print(err)
+        end
+        mp.commandv("seek", data.loc, "absolute+exact")
+        positionFile:close()
+    end
+    
+end)
+
+
+timer = mp.add_periodic_timer(1, function()
+    if isPlaying then
+        position = mp.get_property_number("time-pos")
+    end
+end)
+
+mp.register_event("shutdown", function()
+    isPlaying = false
+    filename = string.gsub(filename, "[^%w%.%-_]", "_")
+    os.execute("mkdir -p " .. folder)
+    filepath = folder .. filename .. ".json"
+    positionFile, err = io.open(filepath, "w")
+    if not positionFile then
+        print("Error opening file to write:", err)
+        return
+    end
+
+    local finalPosition = duration - position
+    if finalPosition <= 5 then
+        position = 0
+    end
+    local data = {
+        loc = position
+    }
+    local str = dkjson.encode(data, { indent = true })
+    positionFile:write(str)
+    positionFile:close()
+end)
+
+
+function getFilename(os)
+    local title = mp.get_property("media-title")
+    local file_format = mp.get_property("file-format")
+    if string.find(file_format, "mp4") then
+        file_format = ".mp4"
+    end
+    local final_filename = title .. file_format
+    return final_filename
+end
+
+function getOS()
+	if jit then
+        print(jit.os)
+		return jit.os
+	end
+
+	local fh,err = assert(io.popen("uname -o 2>/dev/null","r"))
+	if fh then
+		osname = fh:read()
+	end
+
+    myos = osname or "Windows"
+    return myos
+end
